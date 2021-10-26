1b8e8f4e8ea67c482ad3e81934dbb3ccd4f19310
Ronald Arias <ariasron@hotmail.com>
2021-10-25 20:38:53 -0500

    Global Styles - Colors

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,11 +1,27 @@
 import { createGlobalStyle } from 'styled-components'
 
+export const COLORS = {
+  text: '#FFFFFF',
+  background: '#070724',
+  mercury: '#419EBB',
+  venus: '#EDA249',
+  earth: '#6F2ED6',
+  mars: '#D14C32',
+  jupiter: '#D83A34',
+  saturn: '#CD5120',
+  uranus: '#1EC2A4',
+  neptune: '#2D68F0',
+  darkGray: '#38384F',
+  lightGray: '#838391'
+}
+
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Antonio:wght@500&family=Spartan:wght@400;700&display=swap');
   body {
     margin: 0;
     padding: 0;
-    background: #070724;
+    color: ${COLORS.text};
+    background: ${COLORS.background};
     font-family: 'Spartan', sans-serif;
     font-size: 14px;
     font-weight: 400;
