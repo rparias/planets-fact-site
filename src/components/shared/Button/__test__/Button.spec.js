d325362f0ea19f9a75278dbe8db6a620ec923372
Ronald Arias <ariasron@hotmail.com>
2021-10-26 20:25:50 -0500

    Adds a number in the button

--- src/components/shared/Button/__test__/Button.spec.js
+++ src/components/shared/Button/__test__/Button.spec.js
@@ -7,6 +7,7 @@ describe('Button component', () => {
     render(<Button />)
     const button = screen.getByRole('button', { name: /button/i })
     expect(button).toBeInTheDocument()
+    expect(button).toHaveTextContent('01')
     expect(button).not.toHaveClass('active')
   })
 
@@ -14,6 +15,12 @@ describe('Button component', () => {
     render(<Button text="New text" />)
     const button = screen.getByRole('button', { name: /new text/i })
     expect(button).toBeInTheDocument()
+  })
+
+  it('should display a button with the number passed in the parameter', () => {
+    render(<Button number={2} />)
+    const button = screen.getByRole('button', { name: /button/i })
+    expect(button).toHaveTextContent('02')
   })
 
   it('should display a button with the class active if it is passed as parameter', () => {
--- src/components/shared/Button/index.js
+++ src/components/shared/Button/index.js
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { COLORS } from '../../../globalStyles'
 
-const Button = ({ text, isActive, color }) => {
+const Button = ({ text, isActive, color, number }) => {
+  const padNumber = (number = 1) => String(number).padStart(2, '0')
+
   return (
     <ButtonWrapper className={isActive && 'active'} color={color}>
-      {text || 'Button'}
+      {padNumber(number)} {text || 'Button'}
     </ButtonWrapper>
   )
 }
@@ -22,7 +24,8 @@ const ButtonWrapper = styled.button`
 Button.propTypes = {
   text: PropTypes.string,
   isActive: PropTypes.bool,
-  color: PropTypes.string
+  color: PropTypes.string,
+  number: PropTypes.number
 }
 
 export default Button
