c69a99d7173e25a835c3d5e3f2ba78b79f962190
Ronald Arias <ariasron@hotmail.com>
2021-10-28 21:58:04 -0500

    Finishes menubar for desktop

--- src/components/shared/Menubar/_styles.js
+++ src/components/shared/Menubar/_styles.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
+import { COLORS } from '../../../globalStyles'
 
 const Navbar = styled.nav`
   display: flex;
   justify-content: space-between;
+  border-bottom: 1px solid ${COLORS.lightGray};
 
   h2 {
     font-size: 28px;
