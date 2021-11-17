faa47af18436b72e4f0500fe60c51ca5e9c0a4d2
Ronald Arias <ariasron@hotmail.com>
2021-11-17 15:58:37 -0500

    Fixes test passing props to the value provider

--- src/components/__test__/PlanetImage.spec.js
+++ src/components/__test__/PlanetImage.spec.js
@@ -5,31 +5,20 @@ import PlanetImage from '../PlanetImage'
 
 describe('PlanetImage component', () => {
   it('should display an image with the proper alternative text if is passed as prop', () => {
-    const planet = {
-      name: 'Mercury',
-      images: {
-        planet: '/assets/planet-mercury.svg'
-      }
-    }
-    const {
-      name,
-      images: { planet: source }
-    } = planet
     render(<PlanetImage />, { wrapper: PlanetProvider })
     const image = screen.getByRole('img')
     expect(image).toHaveAttribute('alt', 'Mercury')
   })
 
-  it.skip('should display an image with an alternative text if is not passed as prop', () => {
-    const planetProps = {
+  it('should display an image with an alternative text if is not passed as prop', () => {
+    const currentPlanet = {
       name: undefined,
       images: {
         planet: '/assets/planet-mercury.svg'
       }
     }
-    // render(<PlanetImage />, { wrapper: PlanetProvider })
     render(
-      <PlanetContext.Provider {...planetProps}>
+      <PlanetContext.Provider value={{ currentPlanet }}>
         <PlanetImage />
       </PlanetContext.Provider>
     )
@@ -38,14 +27,6 @@ describe('PlanetImage component', () => {
   })
 
   it('should display an image with the proper source', () => {
-    const planet = {
-      images: {
-        planet: '/assets/planet-mercury.svg'
-      }
-    }
-    const {
-      images: { planet: source }
-    } = planet
     render(<PlanetImage />, { wrapper: PlanetProvider })
     const image = screen.getByRole('img')
     expect(image).toHaveAttribute('src', '/assets/planet-mercury.svg')
